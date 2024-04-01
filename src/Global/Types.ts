84458d309b650277792a3873cb00595172ce5752
kesantos <kesantos@paschoalotto.com.br>
2024-04-01 15:41:06 -0300

    refactor: Change types to globa file

--- src/Global/Types.ts
+++ src/Global/Types.ts
@@ -7,3 +7,33 @@ export type RepositoryContextType = {
   repository: string;
   setRepository: (name: string) => void;
 }
+
+export type ProfileCardProps = {
+  name: string;
+  email: string;
+  following: string;
+  followers: string;
+  image: string;
+  bio: string;
+};
+
+export type ProfileResponseType = {
+  name: string;
+  email: string;
+  followers: string;
+  following: string;
+  avatar_url: string;
+  bio?: string | null;
+};
+
+export type RepositoriesResponseType = {
+  name: string;
+  stargazers_count: number;
+  language: string;
+}[];
+
+export type RepositoriesProps = {
+  name: string;
+  stargazers_count: number;
+  language: string;
+};
